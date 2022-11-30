cdd556b6fcc5aac9143ded1e71a487c32f705df9
8cAyqpVKio <116042272+8cAyqpVKio@users.noreply.github.com>
2022-11-30 11:40:10 +0900

    fix(armeabi-v7a): quick fix

--- deps.sh
+++ deps.sh
@@ -6,7 +6,7 @@ pushd "$(cd $(dirname $0);pwd)/third_party"
   #./library_build.sh arm64-v8a android-${ANDROID_MINSDK_VERSION} Debug
   #./library_build.sh x86 android-${ANDROID_MINSDK_VERSION} Debug
   #./library_build.sh x86_64 android-${ANDROID_MINSDK_VERSION} Debug
-  ./library_build.sh armeabi-v7a android-21 Release
+  ./library_build.sh armeabi-v7a android-19 Release
   ./library_build.sh arm64-v8a android-21 Release
   ./library_build.sh x86 android-19 Release
   #./library_build.sh x86_64 android-${ANDROID_MINSDK_VERSION} Release
